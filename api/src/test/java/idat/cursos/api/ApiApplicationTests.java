a40d304546cbfcb295f79728f64dac0eb4c8d7b5
aguilaDePlata <rulicb21@gmail.com>
2023-07-06 22:12:11 -0500

    Commit inicial

---
+++ api/src/test/java/idat/cursos/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.cursos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
